4c54c8dd19f2f10fbdec52eda20cfc6f606f9a9a
joao manoel <joaomanoel.ms.br@gmail.com>
2025-11-05 16:47:18 -0400

    fix views daily

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,9 +3,11 @@ export function formatYYYYMMDDToBR(s: string) {
   const y = Number(s.slice(0, 4))
   const m = Number(s.slice(4, 6)) - 1
   const d = Number(s.slice(6, 8))
+  // cria a data em UTC e também exibe em UTC para não "voltar" um dia no BRT
   const dt = new Date(Date.UTC(y, m, d, 0, 0, 0, 0))
-  // exibe no fuso local do browser
-  return dt.toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit' })
+  return dt.toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    timeZone: 'UTC' // 👈 chave do conserto
+  })
 }
-
-
